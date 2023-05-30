b66a40c9003b705c15ef63422a135383198d31f8
jawn <62079777+jawndiego@users.noreply.github.com>
2023-05-30 19:23:40 -0400

    made changes to server.py and index
    the tailwind form can handle drag & drop + upload via upload flow lol.
    
    upon upload a file is temporarily stored within a folder called temp-file-cache and the browser opens up a google form consent oauth2.0 form.
    
    i am pushing this cuz i had to set an authorized redirect uri to localhost, but it takes 5 mins to a few hours for that shit to take effect. gna go roll somn in the meantime. be ez.

--- src/flask-server/server.py
+++ src/flask-server/server.py
@@ -1,17 +1,48 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import reqparse, Api, Resource
 from flask_cors import CORS
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+import os
 
 app = Flask(__name__)
 CORS(app, resources={r"*": {"origins": "http://localhost:3000"}})
 
-@app.route("/", methods=["GET"])
-def Page():
-    return {"message": "Welcome to the Home page!"}
+def upload_file_to_drive(filename, filepath):
+    gauth = GoogleAuth()
+    gauth.LoadCredentialsFile("credentials.json")
+    if gauth.credentials is None:
+        # Authenticate if they're not there
+        gauth.LocalWebserverAuth()
+    elif gauth.access_token_expired:
+        # Refresh them if expired
+        gauth.Refresh()
+    else:
+        # Initialize the saved creds
+        gauth.Authorize()
+    # Save the current credentials to a file
+    gauth.SaveCredentialsFile("credentials.json")
+    drive = GoogleDrive(gauth)
 
-@app.route("/members", methods=["GET"])
-def members():
-    return {"members": ["Member1", "Member2","Member3"]}
+    gfile = drive.CreateFile({'title': filename})
+    gfile.SetContentFile(filepath)
+    gfile.Upload() # Upload the file.
+    os.remove(filepath) # Once uploaded we remove the file locally.
 
-if __name__ == "__main__":
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    files = request.files.to_dict(flat=False)
+    uploaded_files = files.get('file[]', [])
+    if not uploaded_files:
+        return {"error": "No files part"}, 400
+    for file in uploaded_files:
+        if file.filename == '':
+            return {"error": "No selected file"}, 400
+        filename = file.filename
+        filepath = os.path.join("/Users/lreyes/Desktop/Github/pyfileuploader/src/flask-server/temp-file-cache", filename)
+        file.save(filepath)
+        upload_file_to_drive(filename, filepath)
+    return {"message": "Files successfully uploaded"}, 200
+
+if __name__ == '__main__':
     app.run(debug=True)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,68 @@
 import { Header } from '../components'
 import {useEffect, useState} from 'react'
 
-type MembersType = {
-	members: string[];
-  };
-  
+type MyDragEvent = React.DragEvent<HTMLDivElement>;
+
+
   function Page() {
-	const [data, setData] = useState<MembersType | null>(null);
-  
-	useEffect(() => {
-	  fetch("members")
-		.then(response => {
-		  if (!response.ok) {
-			throw new Error("Error fetching data");
+
+	const onDrop = async (acceptedFiles: File[]): Promise<void> => {
+		try {
+		  const data = new FormData();
+		  
+		  acceptedFiles.forEach((file, index) => {
+			data.append(`file[]`, file); 
+		  });
+	  
+		  const response = await fetch('upload', {
+			method: 'POST',
+			body: data
+		  });
+	  
+		  if (response.ok) {
+			console.log('All files uploaded successfully');
+		  } else {
+			console.log('Upload failed');
 		  }
-		  return response.json();
-		})
-		.then((data: MembersType) => setData(data))
-		.catch((error) => console.error("Error:", error));
-	}, []);
-  
-	if (!data) {
-	  return (
-		<>
-		  <Header />
-		  <p>Loading...</p>
-		</>
-	  );
-	}
-  
+		} catch (error) {
+		  console.error('Error:', error);
+		}
+	  };
+
+	const onFileChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+		if(event.target.files) {
+		  const files = Array.from(event.target.files); // Convert FileList to Array
+		  onDrop(files);
+		}
+	  }
+
 	return (
 	  <>
 		<Header />
 
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-		{/* // TEST API PLACEHOLDER */}
-
-		  {/* <div>
-			{data.members.map((member, i) => (
-			  <p key={i}>{member}</p>
-			))}
-		  </div> */}
-
-		{/* // FORM UPLOAD */}
 		
-          <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+          <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10"
+		    onDragOver={(event: MyDragEvent) => {
+				// Prevent default behavior (Prevent file from being opened)
+				event.preventDefault();
+			  }}
+			  onDrop={(event: MyDragEvent) => {
+				event.preventDefault();
+				const files: File[]= [];
+			  if (event.dataTransfer.items) {
+				for (let i = 0; i < event.dataTransfer.items.length; i++){
+					if (event.dataTransfer.items[i].kind === 'file') {
+						const file = event.dataTransfer.items[i].getAsFile();
+						if (file !== null) {
+							files.push(file);
+						console.log('file', file);}
+					}
+				}
+				onDrop(files);
+			  }
+			}}
+			  >
             <div className="text-center">
               <svg className="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
                 <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd" />
@@ -52,7 +70,9 @@ type MembersType = {
               <div className="mt-4 flex text-sm leading-6 text-gray-600">
                 <label htmlFor="file-upload" className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
                   <span>upload a file</span>
-                  <input id="file-upload" name="file-upload" type="file" accept=".txt,.pdf" className="sr-only"></input>
+				  <input id="file-upload" name="file-upload" type="file" accept=".txt,.pdf" className="sr-only"
+                  onChange={onFileChange}
+                ></input>
                 </label>
                 <p className="pl-1">or drag and drop</p>
               </div>
