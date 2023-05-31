c38c626d80d55116eec251ea1d6b457325a9d185
jawn <62079777+jawndiego@users.noreply.github.com>
2023-05-30 22:30:41 -0400

    updated server.py
    server py now sends to google drive and creates two folders:
    
    -uploads
    -preprocessed uploads
    
    one is for the content the user first uploads then the second folder is for the generated preprocessed upload.
    
    currently the uploaded file is saved, but the second folder is empty because i still need to implement langchain and pinecone.

--- src/flask-server/server.py
+++ src/flask-server/server.py
@@ -8,26 +8,60 @@ import os
 app = Flask(__name__)
 CORS(app, resources={r"*": {"origins": "http://localhost:3000"}})
 
-def upload_file_to_drive(filename, filepath):
+def create_drive_folder(folder_name): 
     gauth = GoogleAuth()
     gauth.LoadCredentialsFile("credentials.json")
     if gauth.credentials is None:
-        # Authenticate if they're not there
         gauth.LocalWebserverAuth()
     elif gauth.access_token_expired:
-        # Refresh them if expired
         gauth.Refresh()
     else:
-        # Initialize the saved creds
         gauth.Authorize()
-    # Save the current credentials to a file
     gauth.SaveCredentialsFile("credentials.json")
     drive = GoogleDrive(gauth)
 
-    gfile = drive.CreateFile({'title': filename})
+    folder_metadata = {'title': folder_name, 'mimeType': 'application/vnd.google-apps.folder'}
+    folder = drive.CreateFile(folder_metadata)
+    folder.Upload()
+
+    # Return the ID of the created folder
+    return folder['id']
+
+# Create the folders once and store the ids
+uploads_folder_id = create_drive_folder("Uploads")
+preprocessed_uploads_folder_id = create_drive_folder("Preprocessed Uploads")
+
+def upload_file_to_drive(filename, filepath, folder_id):
+    gauth = GoogleAuth()
+    gauth.LoadCredentialsFile("credentials.json")
+    if gauth.credentials is None:
+        gauth.LocalWebserverAuth()
+    elif gauth.access_token_expired:
+        gauth.Refresh()
+    else:
+        gauth.Authorize()
+    gauth.SaveCredentialsFile("credentials.json")
+    drive = GoogleDrive(gauth)
+
+    gfile = drive.CreateFile({'title': filename, 'parents': [{'id': folder_id}]})
     gfile.SetContentFile(filepath)
     gfile.Upload() # Upload the file.
-    os.remove(filepath) # Once uploaded we remove the file locally.
+
+def process_text_file(file_path):
+    # Read file content
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    # Process content with langchain
+    preprocessor = TextPreprocessor()
+    processed_text = preprocessor.process_text(content)
+
+    # Save processed content to a new file
+    processed_file_path = os.path.join("/Users/lreyes/Desktop/Github/pyfileuploader/src/flask-server/temp-file-cache", f"{file_path}_processed.txt")
+    with open(processed_file_path, 'w') as file:
+        file.write(processed_text)
+    
+    return processed_file_path
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -35,13 +69,27 @@ def upload_file():
     uploaded_files = files.get('file[]', [])
     if not uploaded_files:
         return {"error": "No files part"}, 400
+
     for file in uploaded_files:
         if file.filename == '':
             return {"error": "No selected file"}, 400
         filename = file.filename
         filepath = os.path.join("/Users/lreyes/Desktop/Github/pyfileuploader/src/flask-server/temp-file-cache", filename)
         file.save(filepath)
-        upload_file_to_drive(filename, filepath)
+
+        # Pass folder_id to upload_file_to_drive function
+        upload_file_to_drive(filename, filepath, uploads_folder_id)
+
+        # Process text file
+        processed_filepath = process_text_file(filepath)
+        processed_filename = f'{filename}_processed.txt'
+        # Upload processed file to the "Preprocessed Uploads" folder
+        upload_file_to_drive(processed_filename, processed_filepath, preprocessed_uploads_folder_id)
+        
+        # Here, you can send the processed files to Pinecone. Remember to remove the files afterwards
+        # send_file_to_pinecone(processed_filepath)
+        os.remove(filepath)
+        os.remove(processed_filepath)
     return {"message": "Files successfully uploaded"}, 200
 
 if __name__ == '__main__':
