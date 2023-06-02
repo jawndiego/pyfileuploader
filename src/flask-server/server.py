8f374421ee7d64085c4959097aeb0fc8f94ed245
jawn <62079777+jawndiego@users.noreply.github.com>
2023-06-02 14:45:56 -0400

    update server.py
    organized code a little nicer, grouped things together, added some comments.
    
    most noticeable change is adding a folder checker, so that duplicate folders arent created across sessions and only one folder for each category is created: "uploads" and "preprocessed uploads"

--- src/flask-server/server.py
+++ src/flask-server/server.py
@@ -11,20 +11,23 @@ from langchain.vectorstores import Pinecone
 from langchain.schema import BaseDocumentTransformer, Document
 from dotenv import load_dotenv, find_dotenv
 import os
-import pinecone
+import pinecone 
 
+# initializes pincone and fetches api key + pinecone env from .env file, user must also provide an index_name 
 pinecone.init(
 api_key=os.getenv('PINECONE_API_KEY'),
 environment=os.getenv('PINECONE_ENV')
         )
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
 index_name="pyneconeapp"
+
+# gets API key from .env
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+#loads env file 
+load_dotenv(find_dotenv())
 
 app = Flask(__name__)
 CORS(app, resources={r"*": {"origins": "http://localhost:3000"}})
-
-load_dotenv(find_dotenv())
 
 def create_drive_folder(folder_name): 
     gauth = GoogleAuth()
@@ -45,10 +48,6 @@ def create_drive_folder(folder_name):
     # Return the ID of the created folder
     return folder['id']
 
-# Create the folders once and store the ids
-uploads_folder_id = create_drive_folder("Uploads")
-preprocessed_uploads_folder_id = create_drive_folder("Preprocessed Uploads")
-
 def upload_file_to_drive(filename, filepath, folder_id):
     gauth = GoogleAuth()
     gauth.LoadCredentialsFile("credentials.json")
@@ -63,8 +62,9 @@ def upload_file_to_drive(filename, filepath, folder_id):
 
     gfile = drive.CreateFile({'title': filename, 'parents': [{'id': folder_id}]})
     gfile.SetContentFile(filepath)
-    gfile.Upload() # Upload the file.
+    gfile.Upload() 
 
+# splits text into chunks using langchain, creates embeddings, sends to pinecone
 def process_text_file(file_path):
     # Read file content
     with open(file_path, 'r') as file:
@@ -75,14 +75,10 @@ def process_text_file(file_path):
     chunk_size=1000, chunk_overlap=20, length_function =len,
 )
     chunks = splitter.split_text(content)
-    # text_splitter = TokenTextSplitter(chunk_size=10, chunk_overlap=0)
-    # texts = text_splitter.split_text(content)
 
     embeddings = OpenAIEmbeddings()
     embeddings_list = embeddings.embed_documents(chunks)
-    # use either chunks or texts .. still getting rate limited
 
-        # pinecone_vector_store = Pinecone(api_key=os.getenv('PINECONE_API_KEY'), index_name=os.getenv("PINECONE_ENV"))
     documents = [Document(page_content=chunk) for chunk in chunks]
     
     index = pinecone.Index(index_name)
@@ -96,14 +92,41 @@ def process_text_file(file_path):
         ]
     )
 
-
-    # Save processed content to a new file
+    # Saves processed content to a new file
     processed_file_path = os.path.join("/Users/lreyes/Desktop/Github/pyfileuploader/src/flask-server/temp-file-cache", f"{file_path}_processed.txt")
     with open(processed_file_path, 'w') as file:
         for embedding in embeddings_list:
             file.write(str(embedding) + "\n")
     
     return processed_file_path
+
+def check_folder_exists(folder_name):
+    gauth = GoogleAuth()
+    gauth.LoadCredentialsFile("credentials.json")
+    if gauth.credentials is None:
+        gauth.LocalWebserverAuth()
+    elif gauth.access_token_expired:
+        gauth.Refresh()
+    else:
+        gauth.Authorize()
+    gauth.SaveCredentialsFile("credentials.json")
+    drive = GoogleDrive(gauth)
+
+    file_list = drive.ListFile({'q': f"title='{folder_name}' and mimeType='application/vnd.google-apps.folder' and trashed=false"}).GetList()
+
+    if file_list:
+        return file_list[0]['id']
+    else:
+        return None
+
+# Check if folders exist and get their IDs, create them if they do not exist, this is to avoid the creation of duplicate folders 
+uploads_folder_id = check_folder_exists("Uploads")
+if not uploads_folder_id:
+    uploads_folder_id = create_drive_folder("Uploads")
+
+preprocessed_uploads_folder_id = check_folder_exists("Preprocessed Uploads")
+if not preprocessed_uploads_folder_id:
+    preprocessed_uploads_folder_id = create_drive_folder("Preprocessed Uploads")
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
